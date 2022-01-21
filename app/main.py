9b9afa9526ac4ec098868b24e9be870be7454e23
Ronald <ronalddavisr3@gmail.com>
2022-01-20 19:35:48 -0500

    added dropdown menu functionality to calculate how big a business chain is

--- app/main.py
+++ app/main.py
@@ -4,17 +4,28 @@ import dash_bootstrap_components as dbc
 from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 from dash.dependencies import Output, Input
+import pandas as pd
+from pathlib import Path
+import os
+path = Path(os.getcwd())
+base_dir = path.parent
+data_dir = os.path.join(base_dir, "data")
+businesses = pd.read_csv(os.path.join(data_dir, "_Production_business.csv"), delimiter='|', quotechar="'")
 
 
-
-# app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app = JupyterDash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+# app = JupyterDash(__name__)
 
 app.layout = html.Div([
     html.H1('Gourmand Database',
         style={'color':'orange',
             'fontSize':'50px'}),
     html.H2('Businesses'),
+    dcc.Dropdown(id = 'chain_dropdown_menu', options=[
+        {'label':chain , 'value':chain} for chain in businesses.iloc[:,2].unique()
+    ])
+    ,html.Br()
+    ,html.Div(id='chain_output'),
     dbc.Tabs([
         dbc.Tab([
             html.Ul([
@@ -48,20 +59,19 @@ app.layout = html.Div([
                 dbc.Col('BusinessID',width=1 ),
                 dbc.Col('BusinessName', width =2)
                 ]
-            ),
-    dcc.Dropdown(id = 'color_dropdown_menu', options=[
-        {'label':color, 'value':color} for color in ['red', 'blue', 'green']
-    ])
-    ,html.Div(id='color_output')
+            )
 ]
 )
 
-@app.callback(Output('color_output', 'children'),
-                Input('color_dropdown_menu', 'value'))
-def display_desired_color(color):
-    if not color:
-        color = 'nothing'
-    return f'Has eligido {color}'
+@app.callback(Output('chain_output', 'children'),
+                Input('chain_dropdown_menu', 'value'))
+def display_desired_chain_cnts(chain):
+    if not chain:
+        return f'{businesses.shape[0]} options to choose from'
+    bus_df = businesses[businesses.iloc[:,2] == chain]
+    chn_cnts = bus_df.shape[0]
+    return [html.H3(chain), f'Has eligido {chain} y el numero de sus sucursales son {chn_cnts} ¿Quien lo iba a decir 😅?']
+
 
 if __name__ == '__main__':
     app.run_server(mode='inline')
