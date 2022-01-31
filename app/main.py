6803b8c608dd72ad457c0e8d73310b1b1ea59e40
Ronald <ronalddavisr3@gmail.com>
2022-01-31 18:51:48 -0500

    conditional probabilities chart just need to edit a bit of char tot function properly

--- app/main.py
+++ app/main.py
@@ -7,23 +7,28 @@ from dash.dependencies import Output, Input
 import pandas as pd
 from pathlib import Path
 import os
+import plotly.graph_objects as go
+import plotly.express as px
+from dash.exceptions import PreventUpdate
+
+
 path = Path(os.getcwd())
 base_dir = path.parent
 data_dir = os.path.join(base_dir, "data")
-businesses = pd.read_csv(os.path.join(data_dir, "_Production_business.csv"), delimiter='|', quotechar="'")
+data_dir
 
+dv_runs = pd.read_csv(os.path.join(data_dir, "dv_runs.csv"), delimiter=',', quotechar='"', header='infer')
+bus_cat_hold_loc_parquet = pd.read_parquet(os.path.join(data_dir, 'bus_cat_hold_loc'), engine='pyarrow')
+df2_groups_pivoted_100_sorted_cut = pd.read_parquet(os.path.join(data_dir, 'df2_groups_pivoted_100_sorted_cut'), engine='pyarrow')
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-# app = JupyterDash(__name__)
+# app = JupyterDash(__name__, external_stylesheets=[dbc.themes.COSMO])
 
 app.layout = html.Div([
     html.H1('Gourmand Database',
         style={'color':'orange',
             'fontSize':'50px'}),
-    html.H2('Businesses'),
-    dcc.Dropdown(id = 'chain_dropdown_menu', options=[
-        {'label':chain , 'value':chain} for chain in businesses.iloc[:,2].unique()
-    ])
+    html.H2('Businesses')
     ,html.Br()
     ,html.Div(id='chain_output'),
     dbc.Tabs([
@@ -40,6 +45,40 @@ app.layout = html.Div([
             ])
     ], label='Data at a Glance'),
         dbc.Tab([
+            html.H2('Daily Review Changes', style={'textAlign': 'center'})
+            ,dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            dcc.Dropdown(
+                                id='year-dropdown'
+                                , options=[{'label': year, 'value': year} for year in bus_cat_hold_loc_parquet['CloseDate'].sort_values().unique()]
+                            )
+                            ,dcc.Graph(
+                                id='review-delta-year-barchart'
+                            )
+                        ]
+                    )
+                    ,dbc.Col(
+                        [
+                            dcc.Dropdown(
+                                id='state-dropdown'
+                                , options=[{'label': state, 'value': state} for state in bus_cat_hold_loc_parquet['StateName'].sort_values().unique()]
+                            )
+                            ,dcc.Graph(
+                                id='review-delta-state-barchart'
+                            )
+                        ]
+                    )                    
+
+                ]
+            )
+            ,html.H2(
+                'hiya!😀'
+                , id='data-heading'
+                )
+        ],label='Stats on States by Day')
+        ,dbc.Tab([
             html.Ul([
                 html.Br()
                 ,html.Li([
@@ -51,27 +90,118 @@ app.layout = html.Div([
                 ])
             ])
         ], label='Background Information')
+    ,dbc.Tab([
+        html.Br()
+        ,dcc.Dropdown(id = 'dag_state_dropdown_menu', options=[
+        {'label':state.title() , 'value':state} for state in list(dv_runs['dag_state'].unique())
     ])
-
-    ,
-    dbc.Row(
-            [
-                dbc.Col('BusinessID',width=1 ),
-                dbc.Col('BusinessName', width =2)
-                ]
+    ,dcc.Dropdown(id = 'date_dropdown_menu',options=[
+        {'label':date , 'value':date} for date in sorted(list(dv_runs['execution_date'].unique()))
+    ])
+    ,dcc.Graph(id='runtime_chart')
+    ], label = 'Airflow Task Graph')
+    ,dbc.Tab(
+        [
+            dcc.Dropdown(
+                id="cond-prob-chart1-dccd"
+                ,options=[{'label':state, 'value':state} for state in df2_groups_pivoted_100_sorted_cut.StateName.unique()]
             )
+            ,dcc.Graph(
+                id='cond-prob-chart1'
+            )
+        ]
+        ,label="Conditional Probabilities"
+    )
+    ]
+    )
+
+    
+#     ,dbc.Row(
+#             [
+#                 dbc.Col('BusinessID',width=1 ),
+#                 dbc.Col('BusinessName', width =2)
+#                 ]
+#             )
 ]
 )
 
-@app.callback(Output('chain_output', 'children'),
-                Input('chain_dropdown_menu', 'value'))
-def display_desired_chain_cnts(chain):
-    if not chain:
-        return f'{businesses.shape[0]} options to choose from'
-    bus_df = businesses[businesses.iloc[:,2] == chain]
-    chn_cnts = bus_df.shape[0]
-    return [html.H3(chain), f'Has eligido {chain} y el numero de sus sucursales son {chn_cnts} ¿Quien lo iba a decir 😅?']
+@app.callback(Output('runtime_chart','figure'),
+             Input('date_dropdown_menu','value'),
+             Input('dag_state_dropdown_menu','value'))
+def plot_by_state_day(date, state):
+    title_date = 'all days'
+    title_state = 'all'
+    if not date and not state:
+        df_sorted = dv_runs.sort_values(['runtime_seconds'], ascending=False)
+    elif state and not date :
+        df = dv_runs.loc[dv_runs['dag_state'] == state]
+        title_state = state
+        df_sorted = df.sort_values(['runtime_seconds'], ascending=False)
+    elif not state and date:
+        df = dv_runs.loc[dv_runs['execution_date'] == date]
+        df_sorted = df.sort_values(['runtime_seconds'], ascending=False)
+        title_date = date
+    else:
+        df = dv_runs.loc[(dv_runs['dag_state'] == state) & (dv_runs['execution_date'] == date)]
+        df_sorted = df.sort_values(['runtime_seconds'], ascending=False)
+        title_state = state
+        title_date = date
+    fig = go.Figure()
+    fig.add_bar(x=df_sorted['dag_id'], y=df_sorted['runtime_seconds'])
+    fig.layout.title = f'airflow tasks runtimes for {title_date} and for {title_state} attempts'
+    fig.layout.yaxis.title = 'runtime (seconds)'
+    fig.layout.template = "ggplot2"
+    return fig
+@app.callback(
+    Output('review-delta-year-barchart', 'figure')
+    ,Input('year-dropdown', 'value')
+)
+def plot_by_year(year):
+    if not year:
+        raise PreventUpdate
+    state_day_groups = bus_cat_hold_loc_parquet.groupby(['StateName', 'CloseDate'], as_index=False).sum()
+    df=state_day_groups[state_day_groups['CloseDate'] == year].sort_values('abs_review_diff').reset_index(drop=True)
+    pixel_constant= df.shape[0]
+    fig = px.bar(df, y='StateName',x='abs_review_diff', title=' - '.join(['abs_review_diff', 'by State']),  orientation='h', height=200 + (20 * pixel_constant))
 
+    return fig
 
-if __name__ == '__main__':
-    app.run_server(mode='inline')
+@app.callback(
+    Output('review-delta-state-barchart', 'figure')
+    ,Input('state-dropdown', 'value')
+)
+def plot_by_state_day(state):
+    if not state:
+        raise PreventUpdate
+    state_day_groups = bus_cat_hold_loc_parquet.groupby(['StateName', 'CloseDate'], as_index=False).sum()
+    df=state_day_groups[state_day_groups['StateName'] == state].sort_values('abs_review_diff').reset_index(drop=True)
+    pixel_constant= df.shape[0]
+    fig = px.bar(df, x='CloseDate',y='abs_review_diff', title=' - '.join([f'abs_review_diff for {state}', 'each day']))
+
+    return fig
+@app.callback(
+    Output('cond-prob-chart1', 'figure')
+    ,Input('cond-prob-chart1-dccd', 'value')
+)
+def cond_prob_1(state):
+    fig = px.bar(df2_groups_pivoted_100_sorted_cut[df2_groups_pivoted_100_sorted_cut['StateName']==state],
+    x=[1,2,3,4,5],
+    y='PaymentLevelName',
+    hover_name='StateName',
+    orientation='h',
+    barmode='stack',
+    height=20 + (20*df2_groups_pivoted_100_sorted_cut[df2_groups_pivoted_100_sorted_cut['StateName']==state].shape[0]),
+    title=f'Ratings by Payment level - {state}')
+    fig.layout.legend.orientation = 'h'
+    fig.layout.legend.title = 'Rating'
+    # fig.layout.legend.title = None
+    fig.layout.xaxis.title = "Percent of Total Ratings"
+    fig.layout.legend.x = 0.25
+    fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+    ))
+    return fig
